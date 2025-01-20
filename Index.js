5c455b1329408853dd37a81bc6fd696f98671703
Benjamin Fernandez Lind <afs0030112@agderskolen.no>
2025-01-20 10:25:29 +0100

    Commit 1

---
+++ Index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get ('/', (req, res) => {
+    res.send('Hello World! klokken er ' + new Date().toLocaleDateString());
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
