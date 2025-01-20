27799fda562a28012c7c1e148d84ff1dc9ea48ae
Benjamin Fernandez Lind <afs0030112@agderskolen.no>
2025-01-20 10:32:02 +0100

    Commit 3

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get ('/', (req, res) => {
-    res.send('Hello World! klokken er ' + new TimeRanges().toLocaleTimeString());
+    res.send('Hello World! klokken er ' + new Date().toLocaleTimeString());
 });
 
 app.listen(3000, () => {
